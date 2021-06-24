3d1980fa57e43651c7f805c38391dc618a4e9f72
Ernest Zulpukarov <retw@dgphoenix.com>
2021-06-24 12:19:38 +0700

    refactor

--- src/main/java/com/siberteam/edu/zernest/ranker/interfaces/IErrorHandler.java
+++
@@ -1,8 +0,0 @@
-package com.siberteam.edu.zernest.ranker.interfaces;
-
-public interface IErrorHandler extends ILogger {
-    default void handleException(Exception e) {
-        log(e + "\n" + e.getCause());
-        System.exit(1);
-    }
-}
--- src/main/java/com/siberteam/edu/zernest/ranker/interfaces/ILogger.java
+++
@@ -1,7 +0,0 @@
-package com.siberteam.edu.zernest.ranker.interfaces;
-
-public interface ILogger {
-    default void log(String message) {
-        System.out.println(message);
-    }
-}
